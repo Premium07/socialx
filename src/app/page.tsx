58295f3c4fff85e6ceee673a43a99260eeebeb9c
Premium07 <premsagarg23@gmail.com>
2025-02-21 14:16:01 +0545

    add utility functions, middleware, and Clerk integration; update Tailwind config and global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Welcome to SociallX.</h1>
-   </div>
+    <div>
+      <h1>Welcome to SocialX.</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>Sign In</button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   );
 }
