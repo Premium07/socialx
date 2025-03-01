d1bdc5f6ebb3aaefd0d61ea8ab848b19d0ce16d5
Premium07 <premsagarg23@gmail.com>
2025-03-01 15:47:55 +0545

    add CreatePost and WhoToFollow components; implement post creation logic with toast notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
+
   return (
-    <div>
-      <h1>Welcome to SocialX.</h1>
-    </div>
+    <section className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <article className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+      </article>
+      <aside className="lg:block hidden lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </aside>
+    </section>
   );
 }
