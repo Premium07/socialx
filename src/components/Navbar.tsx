1eebce82c5a114d69823431f02fc86ec1b3613a4
Premium07 <premsagarg23@gmail.com>
2025-03-01 13:36:25 +0545

    implement user synchronization with Clerk integration and Prisma client setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
+  if (user) await syncUser();
+
   return (
     <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="max-w-7xl mx-auto px-4">
