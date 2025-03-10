7cc666148923f3a2a6c364a42202f472a71e8ddd
Premium07 <premsagarg23@gmail.com>
2025-03-10 16:55:46 +0545

    implement like, comment, and delete comment functionalities in PostCard component

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -73,3 +73,139 @@ export async function getPosts() {
     throw new Error("Failed to fetch posts");
   }
 }
+
+export async function toggleLike(postId: string) {
+  try {
+    const userId = await getDbUserID();
+    if (!userId) return;
+
+    // check if like exists
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        postId_userId: {
+          userId,
+          postId,
+        },
+      },
+    });
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: {
+        authorId: true,
+      },
+    });
+
+    if (!post) throw new Error("Post not found");
+
+    if (existingLike) {
+      //unlike
+      await prisma.like.delete({
+        where: {
+          postId_userId: {
+            userId,
+            postId,
+          },
+        },
+      });
+    } else {
+      // like and create notification (only if liking someone's post)
+      await prisma.$transaction([
+        prisma.like.create({
+          data: {
+            userId,
+            postId,
+          },
+        }),
+        ...(post.authorId !== userId
+          ? [
+              prisma.notification.create({
+                data: {
+                  type: "LIKE",
+                  userId: post.authorId, // post author
+                  creatorId: userId, //person who liked
+                  postId,
+                },
+              }),
+            ]
+          : []),
+      ]);
+    }
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.log("Failed to like:", error);
+    return { sucess: false, error: "Failed to like" };
+  }
+}
+
+export async function createComment(postId: string, content: string) {
+  try {
+    const userId = await getDbUserID();
+    if (!userId) return;
+    if (!content) throw new Error("Content is required");
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Post not found");
+    // create comment and notification in transaction
+
+    const [comment] = await prisma.$transaction(async (tx) => {
+      // create comment
+      const newComment = await tx.comment.create({
+        data: {
+          content,
+          authorId: userId,
+          postId,
+        },
+      });
+
+      // create notification if commenting on someone's else post
+      if (post.authorId !== userId) {
+        await tx.notification.create({
+          data: {
+            type: "COMMENT",
+            userId: post.authorId,
+            creatorId: userId,
+            postId,
+            commentId: newComment.id,
+          },
+        });
+      }
+      return [newComment];
+    });
+
+    revalidatePath("/");
+    return { success: true, comment };
+  } catch (error) {
+    console.log("failed to comment:", error);
+    return { success: false, error: "Failed to comment on post." };
+  }
+}
+
+export async function deleteComment(postId: string) {
+  try {
+    const userId = await getDbUserID();
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Post not found");
+    if (post.authorId !== userId)
+      throw new Error("Unauthorized-no delete permission");
+
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.log("Failed to delete post", error);
+    return { success: false, error: "Failed to delete post." };
+  }
+}
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,47 @@
-const PostCard = ({ post, dbUserId }) => {
+"use client";
+
+import { getPosts, toggleLike } from "@/actions/post.action";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+
+const PostCard = ({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) => {
+  const { user } = useUser();
+  const [comment, setComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(
+    post.likes.some((like) => like.userId === dbUserId)
+  );
+  const [likes, setLikes] = useState(post._count.likes);
+
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setLikes((prev) => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
+    }
+  };
+
+  const handleAddComment = async () => {};
+  const handleDeleteComment = async () => {};
+
   return <div>PostCard</div>;
 };
 
